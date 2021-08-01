81cf119908047670439eb89171e81fcc095f3cf6
Alexey Kim <kmlx@yandex.ru>
2021-08-01 23:44:59 +0300

    Added delete_tree_element

--- binary_search.py
+++ binary_search.py
@@ -28,7 +28,7 @@ def get_tree_element(tree, element):
             return None
 
 
-# {root: 0, left_child: NONE, right_child: {root: 1, left_child: NONE, right_child: NONE}}
+# {root: 0, left_child: {root: -1, left_child: {root: -2}, right_child: None}, right_child: {root: 1, left_child: NONE, right_child: NONE}}
 def put_tree_element(tree, element):
     if tree.get('root') is not None:
         if element >= tree['root']:
@@ -42,4 +42,34 @@ def put_tree_element(tree, element):
     else:
         tree['root'] = element
 
+
 # Операция удаления..
+def delete_tree_element(tree, element):
+    if tree.get('root') is not None:
+        if tree['root'] == element:
+            if tree.get('right_child') is None and tree.get(
+                    'left_child') is None:
+                tree.pop('root')
+                return
+            if tree.get('right_child') is None and tree.get(
+                    'left_child') is not None:
+                tree = tree['left_child']
+                return
+            if tree.get('right_child') is not None and tree.get(
+                    'left_child') is None:
+                tree = tree['right_child']
+                return
+            else:
+                tree['root'] = pop_min_element(tree['right_child'])
+        else:
+            if element >= tree['root'] and 'right_child' in tree:
+                delete_tree_element(tree['right_child'], element)
+            if element < tree['root'] and 'left_child' in tree:
+                delete_tree_element(tree['right_child'], element)
+
+
+def pop_min_element(tree):
+    if 'left_child' not in tree:
+        element = tree['root']
+        return element
+    return pop_min_element(tree['left_child'])
--- test_tree.py
+++ test_tree.py
@@ -1,4 +1,5 @@
 from binary_search import make_bin_tree, get_tree_element, put_tree_element
+from binary_search import delete_tree_element
 
 
 def test_tree():
@@ -21,3 +22,17 @@ def test_tree():
     assert get_tree_element(tree, 0) == 0
     assert get_tree_element(tree, -2) == -2
     assert get_tree_element(tree, 2) == 2
+    put_tree_element(tree, 1.5)
+    put_tree_element(tree, 3)
+    put_tree_element(tree, 2.5)
+    put_tree_element(tree, 2.4)
+    delete_tree_element(tree, 2)
+    assert get_tree_element(tree, 1) == 1
+    assert get_tree_element(tree, 1) == 1
+    assert get_tree_element(tree, 0) == 0
+    assert get_tree_element(tree, -2) == -2
+    assert get_tree_element(tree, 2) is None
+    assert get_tree_element(tree, 1.5) == 1.5
+    assert get_tree_element(tree, 3) == 3
+    assert get_tree_element(tree, 2.5) == 2.5
+    assert get_tree_element(tree, 2.4) == 2.4
